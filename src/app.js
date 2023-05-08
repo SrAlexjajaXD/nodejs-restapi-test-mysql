c2d54f49d096529fc4d76601f8e21bf5ea292754
SrAlexjajaXD <alexloga18@gmail.com>
2023-05-08 16:46:03 -0600

    se agrego body parser

--- src/app.js
+++ src/app.js
@@ -1,10 +1,23 @@
 import express from "express"
 import docentesRoutes from "./routes/docentes.routes.js";
 import alumnosRoutes from "./routes/alumnos.routes.js";
+import bodyParser from "body-parser";
 
 const app = express()
 
-app.use(express.json())
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
+    next();
+});
 
 app.use(alumnosRoutes)
 app.use(docentesRoutes)
--- src/index.js
+++ src/index.js
@@ -1,14 +1,8 @@
 import app from "./app.js"
 import { PORT } from "./config.js"
-import express from 'express'
 
-app.use(express.json())
 
-app.use(function(req,res,next){
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-Width, Content-Type, Accept");
-    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE");
-    next();
-});
+
+
 app.listen(PORT)
 console.log("server running on port", PORT)
