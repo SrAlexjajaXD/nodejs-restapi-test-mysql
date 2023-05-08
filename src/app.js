7cb098b01db20070d6d8aaff0c4de5e3075ab5f2
SrAlexjajaXD <alexloga18@gmail.com>
2023-05-07 19:56:20 -0600

    bodypasrse.optionjson se agrego en la linea 7

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import alumnosRoutes from "./routes/alumnos.routes.js";
 
 const app = express()
 
-app.use(express.json())
+app.use(express.json(bodyParse.OptopnsJson))
 
 app.use(alumnosRoutes)
 app.use(docentesRoutes)
